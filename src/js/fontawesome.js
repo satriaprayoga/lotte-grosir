bd3a427efe986545662a8191d6cb510ce92980d8
Gilang Satria <satria.prayoga@gmail.com>
2018-08-03 02:07:20 +0700

    layout

--- src/js/fontawesome.js
+++ src/js/fontawesome.js
@@ -4,5 +4,7 @@ const faCogs = require("@fortawesome/fontawesome-free-solid/facogs");
 const faEdit = require("@fortawesome/fontawesome-free-regular/faedit");
 const faMap=require("@fortawesome/fontawesome-free-regular/faMap");
 const faFacebook = require("@fortawesome/fontawesome-free-brands/faFacebook");
-
-fontawesome.library.add(faGithub, faCogs, faEdit, faFacebook,faMap);
+const faTwitter = require("@fortawesome/fontawesome-free-brands/faTwitter");
+const faYoutube = require("@fortawesome/fontawesome-free-brands/faYoutube");
+const faInstagram= require("@fortawesome/fontawesome-free-brands/faInstagram");
+fontawesome.library.add(faInstagram, faYoutube, faTwitter, faFacebook,faMap);
--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,21 @@
         var hamburger=document.getElementsByClassName('hamburger')[0];
         hamburger.classList.toggle('change');
     });
-  
-
+    /* $(window).on("activate.bs.scrollspy", function (e,obj){  
+        if(obj.relatedTarget.length>0){
+            $('.navbar').toggleClass('fixed-top');
+          
+        }
+        refreshScrollSpy();
+    }); */
     $('.domisili').select2();
+    $('.media').select2();
+
+    
 
 }(jQuery));
+function refreshScrollSpy() {
+    $('[data-spy="scroll"]').each(function () {
+        $(this).scrollspy('refresh');
+    }); 
+};
